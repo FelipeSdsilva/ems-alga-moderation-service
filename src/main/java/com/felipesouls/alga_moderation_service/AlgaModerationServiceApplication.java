f52466732098c38abd09f1b8a083f7153b1ff882
Felipe Sousa <felipe.fps09@hotmail.com>
2026-05-06 12:14:34 -0300

    Feat: Start project

---
+++ src/main/java/com/felipesouls/alga_moderation_service/AlgaModerationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.felipesouls.alga_moderation_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgaModerationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgaModerationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/felipesouls/alga_moderation_service/AlgaModerationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipesouls.alga_moderation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgaModerationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
